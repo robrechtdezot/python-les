8ff64d3a7f7a98eabe9f664c6cb5169051109bfa
Wotto <woutservaes1@gmail.com>
2025-03-27 02:02:57 +0100

    l

--- les1/test.py
+++ les1/test.py
@@ -1,8 +1,15 @@
-def new_func():
-    def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
 
-new_func()
+investment_in_bitcoin = 1.2
+bitcoin_to_usd = 40000
+
+bitcoin_amount = 1.2
+bitcoin_to_usd = 40000
+def calculate_bitcoin_to_usd(bitcoin_amount, bitcoin_to_usd):
+   bitcoinToUSD  = bitcoin_amount * bitcoin_to_usd
+  return investment_in_usd
+
+bitcoin_value_usd = calculate_bitcoin_to_usd(bitcoin_amount, bitcoin_to_usd)
+if bitcoin_value_usd <= 30000:
+  print("Investment below $30,000! SELL!")
+else:
+  print("Investment above $30,000")
